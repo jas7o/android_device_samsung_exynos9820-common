3cd4362fb88b5c6523eedc30e940288fde963a8e
jas7o <jasio2k00@gmail.com>
2022-07-13 20:26:12 +0100

    ANT: ANT stack extra files

--- common.mk
+++ common.mk
@@ -54,8 +54,12 @@ TARGET_EXCLUDES_AUDIOFX := true
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
+    com.dsi.ant.antradio_library \
     com.dsi.ant@1.0.vendor
 
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl:64 \
